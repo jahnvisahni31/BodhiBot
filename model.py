7792a69004d33a8dc1886a6d7d7c05432bb7847c
Jahnvi sahni <121384173+jahnvisahni31@users.noreply.github.com>
2024-08-24 20:52:13 +0530

    Update model.py

--- model.py
+++ model.py
@@ -80,7 +80,7 @@ async def start():
     chain = qa_bot()
     msg = cl.Message(content="Starting the bot...")
     await msg.send()
-    msg.content = "Hi, Welcome to Medical Bot. What is your query?"
+    msg.content = "Hi, Welcome to BodhiBot. What is your query?"
     await msg.update()
 
     cl.user_session.set("chain", chain)
